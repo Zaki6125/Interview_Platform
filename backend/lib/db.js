30179fed6eaf61d8bb609c401c409e70885592b9
Zaki6125 <muhammadzakii.CS@gmail.com>
2025-11-09 01:40:53 -0800

    Database

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -3,6 +3,10 @@ import {ENV} from './env.js'
 
 export const connectDB = async ()=>{
     try {
+        if(!ENV.DB_URL)
+        {
+            throw new error('DB_URL is not valid')
+        }
         const conn = await mongoose.connect(ENV.DB_URL)
         console.log('This is  mongoDB connection' , conn.connection.host)
     } catch (error) {
